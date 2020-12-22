dff50e87a3ca04fe22d2e9d22e50ab28627c92f9
Kevin Kondratuk <k.kondratuk@fetchrewards.com>
2020-12-22 15:52:15 -0600

    add some logic to solve for day 1 part 2

--- day-1/main.go
+++ day-1/main.go
@@ -28,15 +28,38 @@ func main() {
 		stash := k
 		// delete the current value, then make a new list of values for the remainder of the list
 		delete(values, k)
-		remaining := make(map[int64]interface{})
-		for k, v := range values {
-			remaining[k] = v
-			if stash + k == 2020 {
-				log.Printf("%d plus %d equals %d\n", stash, k, stash+k)
+		remaining := newSetMinusCurrent(values, k)
+
+		for key, val := range remaining {
+			remaining[key] = val
+			if stash +key == 2020 {
+				log.Printf("two-number-matching: %d plus %d equals %d\n", stash, key, stash+key)
 
 				// if the values add to 2020, multiply them to see what they come to
-				log.Printf("Multiplied that is: %d\n", stash * k)
+				log.Printf("two-number-matching: Multiplied that is: %d\n", stash *key)
+			} else {
+				// try adding a 3rd number to see if we can get to 2020 with another one
+				// kind of a mess and it will give us duplicates because we're not removing the value, but it works
+				r2remaining := newSetMinusCurrent(remaining, key)
+				for key2, _ := range r2remaining {
+					if stash + key + key2 == 2020 {
+						log.Printf("three-number-matching: %d plus %d plus %d equals %d\n", stash, key, key2, stash + key + key2)
+						log.Printf("three-number-matching: Multiplied that is: %d\n", stash * key * key2)
+					}
+				}
 			}
 		}
 	}
+}
+
+func newSetMinusCurrent(set map[int64]interface{}, current int64) map[int64]interface{} {
+	newSet := make(map[int64]interface{})
+
+	for k, v := range set {
+		if k != current {
+			newSet[k] = v
+		}
+	}
+
+	return newSet
 }
---
+++ day-2/go.mod
@@ -0,0 +1,3 @@
+module github.com/kjkondratuk/2020-advent-of-code/day-2
+
+go 1.15
