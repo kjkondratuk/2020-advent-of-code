b87698e76f3e1d3654ca68896acec8ec95923696
Kevin Kondratuk <k.kondratuk@fetchrewards.com>
2020-12-22 18:46:35 -0600

    complete day 2 part 2

--- day-2/main.go
+++ day-2/main.go
@@ -19,19 +19,33 @@ func main() {
 
 	total := 0
 	totalPassing := 0
+	totalPassing2 := 0
 
 	for _, line := range lines {
 		total++
 		low, high, c, password := deserializeLine(line)
 		//log.Printf("low: %d - high: %d - char: %s - pass: %s", low, high, c, password)
+
+		// Count values for first set of criteria
 		count := int64(strings.Count(password, c))
 
 		if count >= low && count <= high {
 			totalPassing++
-			log.Printf("%s meets criteria: %d <= %d <= %d", password, low, count, high)
+			//log.Printf("%s meets part 1 criteria: %d <= %d <= %d", password, low, count, high)
+		}
+
+		// Count values for second set of criteria
+		firstRune := string([]rune(password)[low -1])
+		secondRune := string([]rune(password)[high -1])
+
+		log.Printf("Comparing: %s(%d) to %s and %s(%d) to %s in %s\n", firstRune, low-1, c, secondRune, high-1, c, password)
+		if firstRune == c && !(secondRune == c) || !(firstRune == c) && secondRune == c {
+			totalPassing2++
+			//log.Printf("Match found!\n")
 		}
 	}
-	log.Printf("Total is: %d of totalPassing %d", totalPassing, total)
+	log.Printf("Total meeting part 1 criteria is: %d of totalPassing %d", totalPassing, total)
+	log.Printf("Total meeting part 2 criteria is: %d of totalPassing %d", totalPassing2, total)
 }
 
 func deserializeLine(line string) (low int64, high int64, c string, password string) {
